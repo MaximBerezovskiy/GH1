58c488f0212f355fba8b665688edeac1497c1f95
MaximBerezovskiy <maxim.berezovskiy@yahoo.com>
2014-01-16 12:52:24 +0200

    test

---
+++ src/Clazz1.java
@@ -0,0 +1,14 @@
+/**
+ *
+ */
+
+/**
+ * <b>Description:</b> This class ...
+ * <br/><br/><b>Data Model Entity:</b> none
+ * <br/><br/><b>Configuration:</b> none
+ * <br/><br/><b>Known Issues:</b> none
+ *
+ * @author Maxim Berezovskiy
+ */
+public class Clazz1 {
+}
